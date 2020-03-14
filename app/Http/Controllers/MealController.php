404025650912f62fb60f7b868e065d2637b317b1
Abd Al-Gader Sirag <38109544+abdalgaderserag@users.noreply.github.com>
2020-03-14 06:24:00 -0700

    create the meal controller (not tested yet)

--- app/Http/Controllers/MealController.php
+++ app/Http/Controllers/MealController.php
@@ -2,9 +2,90 @@
 
 namespace App\Http\Controllers;
 
+use App\Meal;
 use Illuminate\Http\Request;
 
 class MealController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $menu = Meal::all();
+        return response($menu);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function create()
+    {
+        return view('');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $meal = new Meal($request->except(''));
+        $meal->save();
+        return redirect()->action('');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Meal  $meal
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Meal $meal)
+    {
+        return response($meal);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Meal  $meal
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Meal $meal)
+    {
+        return view('');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Meal  $meal
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Meal $meal)
+    {
+        $meal->update($request->except(''));
+        $meal->save();
+        return redirect()->action('');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Meal  $meal
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Meal $meal)
+    {
+        $meal->delete();
+        return redirect()->action('');
+    }
 }
