79edda4bcc49eb2302644d1a217ea70021fd1f26
Abd Al-Gader Sirag <38109544+abdalgaderserag@users.noreply.github.com>
2020-04-18 12:38:45 -0700

    finish base stage going to design

--- database/factories/MealFactory.php
+++ database/factories/MealFactory.php
@@ -5,7 +5,7 @@ use Faker\Generator as Faker;
 $factory->define(App\Meal::class, function (Faker $faker) {
     return [
         'title' => $faker->jobTitle,
-        'price' => (random_int(5,24)*10) + 0.50,
+        'price' => (random_int(5,24)*100) + 0.50,
 //        'time' => $faker->time(),
         'describe' => $faker->paragraph,
         'image' => ''
