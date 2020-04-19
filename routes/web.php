25746a76e53013b177a8445370ec9d51e4fa0b1a
Abd Al-Gader Sirag <38109544+abdalgaderserag@users.noreply.github.com>
2020-04-19 01:12:50 -0700

    add auth controllers

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::resource('/menu','MealController');
 //Auth::routes();
 
 //Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+//Route::get('/home', 'HomeController@index')->name('home');
